80e3b7861e9d9e157c8f0148796054ac723b84b1
Jérémy Jarrié <jeremy.jarrie@sensiolabs.com>
2019-06-10 10:43:56 +0200

    Fix composer.json, adding exception rethrowing and fix CI

--- src/Facebook/HttpClients/FacebookSymfonyHttpClient.php
+++ src/Facebook/HttpClients/FacebookSymfonyHttpClient.php
@@ -2,7 +2,9 @@
 
 namespace Facebook\HttpClients;
 
+use Facebook\Exceptions\FacebookSDKException;
 use Facebook\Http\GraphRawResponse;
+use Symfony\Component\HttpClient\Exception\TransportException;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
@@ -13,7 +15,7 @@ class FacebookSymfonyHttpClient implements FacebookHttpClientInterface
     public function __construct(HttpClientInterface $httpClient = null)
     {
         if (null === $httpClient && !class_exists('Symfony\Component\HttpClient\HttpClient')) {
-            throw new \LogicException('You cannot use the "Facebook\HttpClients\FacebookSymfonyHttpClient" as no class implement "Symfony\Contracts\HttpClient\HttpClientInterface" have been provided. Try running "composer require symfony/http-client".');
+            throw new \LogicException('You cannot use the "Facebook\HttpClients\FacebookSymfonyHttpClient" as no class implementing "Symfony\Contracts\HttpClient\HttpClientInterface" has been provided. Try running "composer require symfony/http-client".');
         }
 
         $this->httpClient = $httpClient ?: HttpClient::create();
@@ -21,7 +23,11 @@ class FacebookSymfonyHttpClient implements FacebookHttpClientInterface
 
     public function send($url, $method, $body, array $headers, $timeOut)
     {
-        $response = $this->httpClient->request($method, $url, ['headers' => $headers, 'timeout' => $timeOut]);
+        try {
+            $response = $this->httpClient->request($method, $url, ['headers' => $headers, 'timeout' => $timeOut]);
+        } catch (TransportException $e) {
+            throw new FacebookSDKException($e->getMessage(), $e->getCode());
+        }
 
         return new GraphRawResponse($response->getHeaders(), $response->getContent(), $response->getStatusCode());
     }
